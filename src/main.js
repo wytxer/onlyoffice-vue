0605810c0819047e685427691be7344033b7e1ca
wytxer <wy@bszhct.com>
2022-09-16 20:56:49 +0800

    refactor：按需加载 antd

--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
 import Vue from 'vue'
-import { Button } from 'ant-design-vue'
+import { Modal, message, notification, Button } from 'ant-design-vue'
 import App from '@/app.vue'
 import router from '@/router'
+import LayoutBox from '@/components/layout-box'
 import OnlyofficeEditor from '@/components'
-import ExCard from '@/components/ex-card'
 
 import '@wytxer/style-utils/lib/common.less'
 import 'ant-design-vue/dist/antd.less'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$confirm = Modal.confirm
+Vue.prototype.$message = message
+Vue.prototype.$notification = notification
+Vue.prototype.$info = Modal.info
+Vue.prototype.$success = Modal.success
+Vue.prototype.$error = Modal.error
+Vue.prototype.$warning = Modal.warning
+
 Vue.use(Button)
+Vue.use(LayoutBox)
 Vue.use(OnlyofficeEditor)
-Vue.use(ExCard)
 
 new Vue({
   router,
